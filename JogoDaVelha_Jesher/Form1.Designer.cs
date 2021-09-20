b57f22a26a39d7fa9e75804091d2d990f8c18328
Jesher Minelli <jesherdevsk8@gmail.com>
2021-09-20 17:09:15 -0300

    Envia Jogo da velha c#

---
+++ JogoDaVelha_Jesher/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace JogoDaVelha_Jesher
+{
+    partial class Frm01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_11 = new System.Windows.Forms.Button();
+            this.btn_12 = new System.Windows.Forms.Button();
+            this.btn_13 = new System.Windows.Forms.Button();
+            this.btn_22 = new System.Windows.Forms.Button();
+            this.btn_21 = new System.Windows.Forms.Button();
+            this.btn_23 = new System.Windows.Forms.Button();
+            this.btn_31 = new System.Windows.Forms.Button();
+            this.btn_32 = new System.Windows.Forms.Button();
+            this.btn_33 = new System.Windows.Forms.Button();
+            this.btn_NnovoJogo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_11
+            // 
+            this.btn_11.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_11.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_11.Location = new System.Drawing.Point(33, 34);
+            this.btn_11.Name = "btn_11";
+            this.btn_11.Size = new System.Drawing.Size(76, 70);
+            this.btn_11.TabIndex = 0;
+            this.btn_11.UseVisualStyleBackColor = false;
+            this.btn_11.Click += new System.EventHandler(this.btn_11_Click);
+            // 
+            // btn_12
+            // 
+            this.btn_12.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_12.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btn_12.Location = new System.Drawing.Point(145, 34);
+            this.btn_12.Name = "btn_12";
+            this.btn_12.Size = new System.Drawing.Size(76, 70);
+            this.btn_12.TabIndex = 1;
+            this.btn_12.UseVisualStyleBackColor = false;
+            this.btn_12.Click += new System.EventHandler(this.btn_12_Click);
+            // 
+            // btn_13
+            // 
+            this.btn_13.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_13.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btn_13.Location = new System.Drawing.Point(259, 34);
+            this.btn_13.Name = "btn_13";
+            this.btn_13.Size = new System.Drawing.Size(76, 70);
+            this.btn_13.TabIndex = 2;
+            this.btn_13.UseVisualStyleBackColor = false;
+            this.btn_13.Click += new System.EventHandler(this.btn_13_Click);
+            // 
+            // btn_22
+            // 
+            this.btn_22.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_22.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btn_22.Location = new System.Drawing.Point(145, 113);
+            this.btn_22.Name = "btn_22";
+            this.btn_22.Size = new System.Drawing.Size(76, 70);
+            this.btn_22.TabIndex = 3;
+            this.btn_22.UseVisualStyleBackColor = false;
+            this.btn_22.Click += new System.EventHandler(this.btn_22_Click);
+            // 
+            // btn_21
+            // 
+            this.btn_21.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_21.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btn_21.Location = new System.Drawing.Point(33, 113);
+            this.btn_21.Name = "btn_21";
+            this.btn_21.Size = new System.Drawing.Size(76, 70);
+            this.btn_21.TabIndex = 4;
+            this.btn_21.UseVisualStyleBackColor = false;
+            this.btn_21.Click += new System.EventHandler(this.btn_21_Click);
+            // 
+            // btn_23
+            // 
+            this.btn_23.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_23.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btn_23.Location = new System.Drawing.Point(259, 113);
+            this.btn_23.Name = "btn_23";
+            this.btn_23.Size = new System.Drawing.Size(76, 70);
+            this.btn_23.TabIndex = 5;
+            this.btn_23.UseVisualStyleBackColor = false;
+            this.btn_23.Click += new System.EventHandler(this.btn_23_Click);
+            // 
+            // btn_31
+            // 
+            this.btn_31.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_31.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btn_31.Location = new System.Drawing.Point(33, 191);
+            this.btn_31.Name = "btn_31";
+            this.btn_31.Size = new System.Drawing.Size(76, 70);
+            this.btn_31.TabIndex = 6;
+            this.btn_31.UseVisualStyleBackColor = false;
+            this.btn_31.Click += new System.EventHandler(this.btn_31_Click);
+            // 
+            // btn_32
+            // 
+            this.btn_32.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_32.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btn_32.Location = new System.Drawing.Point(145, 191);
+            this.btn_32.Name = "btn_32";
+            this.btn_32.Size = new System.Drawing.Size(76, 70);
+            this.btn_32.TabIndex = 7;
+            this.btn_32.UseVisualStyleBackColor = false;
+            this.btn_32.Click += new System.EventHandler(this.btn_32_Click);
+            // 
+            // btn_33
+            // 
+            this.btn_33.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_33.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btn_33.Location = new System.Drawing.Point(259, 191);
+            this.btn_33.Name = "btn_33";
+            this.btn_33.Size = new System.Drawing.Size(76, 70);
+            this.btn_33.TabIndex = 8;
+            this.btn_33.UseVisualStyleBackColor = false;
+            this.btn_33.Click += new System.EventHandler(this.btn_33_Click);
+            // 
+            // btn_NnovoJogo
+            // 
+            this.btn_NnovoJogo.BackColor = System.Drawing.Color.OrangeRed;
+            this.btn_NnovoJogo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_NnovoJogo.Font = new System.Drawing.Font("Segoe UI Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_NnovoJogo.ForeColor = System.Drawing.Color.SpringGreen;
+            this.btn_NnovoJogo.Location = new System.Drawing.Point(126, 292);
+            this.btn_NnovoJogo.Name = "btn_NnovoJogo";
+            this.btn_NnovoJogo.Size = new System.Drawing.Size(95, 28);
+            this.btn_NnovoJogo.TabIndex = 9;
+            this.btn_NnovoJogo.Text = "Novo Jogo";
+            this.btn_NnovoJogo.UseVisualStyleBackColor = false;
+            this.btn_NnovoJogo.Click += new System.EventHandler(this.btn_NnovoJogo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 343);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Desenvolvido por Jesher Minelli Alves";
+            // 
+            // Frm01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(364, 365);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_NnovoJogo);
+            this.Controls.Add(this.btn_33);
+            this.Controls.Add(this.btn_32);
+            this.Controls.Add(this.btn_31);
+            this.Controls.Add(this.btn_23);
+            this.Controls.Add(this.btn_21);
+            this.Controls.Add(this.btn_22);
+            this.Controls.Add(this.btn_13);
+            this.Controls.Add(this.btn_12);
+            this.Controls.Add(this.btn_11);
+            this.Name = "Frm01";
+            this.Text = "Jogo Da Velha - JESHER";
+            this.Load += new System.EventHandler(this.Frm01_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_11;
+        private System.Windows.Forms.Button btn_12;
+        private System.Windows.Forms.Button btn_13;
+        private System.Windows.Forms.Button btn_22;
+        private System.Windows.Forms.Button btn_21;
+        private System.Windows.Forms.Button btn_23;
+        private System.Windows.Forms.Button btn_31;
+        private System.Windows.Forms.Button btn_32;
+        private System.Windows.Forms.Button btn_33;
+        private System.Windows.Forms.Button btn_NnovoJogo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JogoDaVelha_Jesher/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;     // <-- Adicionado para Usar o SoundPlayer
+
+namespace JogoDaVelha_Jesher
+{
+    public partial class Frm01 : Form
+    {      
+
+        string xo = "X";
+
+        private SoundPlayer Tocador;
+
+        public Frm01()
+        {
+            InitializeComponent();
+
+            Tocador = new SoundPlayer();   // o audio foi adicionado na pasta do projeto em /bin/debug
+        }
+
+        private void fimdejogo()
+        {
+            btn_11.Enabled = false; btn_12.Enabled = false; btn_13.Enabled = false;
+            btn_21.Enabled = false; btn_22.Enabled = false; btn_23.Enabled = false;
+            btn_31.Enabled = false; btn_32.Enabled = false; btn_33.Enabled = false;
+        }
+
+        public void limpar()
+        {
+            xo = "X";
+            btn_11.Text = ""; btn_12.Text = ""; btn_13.Text = "";
+            btn_21.Text = ""; btn_22.Text = ""; btn_23.Text = "";
+            btn_31.Text = ""; btn_32.Text = ""; btn_33.Text = "";
+
+            btn_11.Enabled = true; btn_12.Enabled = true; btn_13.Enabled = true;
+            btn_21.Enabled = true; btn_22.Enabled = true; btn_23.Enabled = true;
+            btn_31.Enabled = true; btn_32.Enabled = true; btn_33.Enabled = true;
+
+        }
+
+        // Método que Verifica se alguém venceu ou s eo jogo terminou Empatado
+        private void VerificandoStatus()
+        {
+            // Verificando se X Ganhou
+
+            if (btn_11.Text == "X" && btn_12.Text == "X" && btn_13.Text == "X"
+             || btn_21.Text == "X" && btn_22.Text == "X" && btn_23.Text == "X"
+             || btn_31.Text == "X" && btn_32.Text == "X" && btn_33.Text == "X")
+            {
+                Tocador.SoundLocation = "Video_Game_Win.WAV";
+                Tocador.Play();
+                MessageBox.Show("O Jogador X Venceu", "Ganhou!!!",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fimdejogo();
+                return;
+            }
+
+            if (btn_11.Text == "X" && btn_21.Text == "X" && btn_31.Text == "X"
+             || btn_12.Text == "X" && btn_22.Text == "X" && btn_32.Text == "X"
+             || btn_13.Text == "X" && btn_23.Text == "X" && btn_33.Text == "X")
+            {
+                Tocador.SoundLocation = "Video_Game_Win.WAV";
+                Tocador.Play();
+                MessageBox.Show("O Jogador X Venceu", "Ganhou!!!",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fimdejogo();
+                return;
+            }
+
+            if (btn_11.Text == "X" && btn_22.Text == "X" && btn_33.Text == "X"
+              || btn_13.Text == "X" && btn_22.Text == "X" && btn_31.Text == "X")
+            {
+                Tocador.SoundLocation = "Video_Game_Win.WAV";
+                Tocador.Play();
+                MessageBox.Show("O Jogador X Venceu", "Ganhou!!!",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fimdejogo();
+                return;
+            }
+
+            // Verificando se O Ganhou
+
+            if (btn_11.Text == "O" && btn_12.Text == "O" && btn_13.Text == "O"
+             || btn_21.Text == "O" && btn_22.Text == "O" && btn_23.Text == "O"
+             || btn_31.Text == "O" && btn_32.Text == "O" && btn_33.Text == "O")
+            {
+                Tocador.SoundLocation = "Error-Eletric.WAV";
+                Tocador.Play();
+                MessageBox.Show("O Jogador O Venceu", "Ganhou!!!",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fimdejogo();
+                return;
+            }
+
+            if (btn_11.Text == "O" && btn_21.Text == "O" && btn_31.Text == "O"
+             || btn_12.Text == "O" && btn_22.Text == "O" && btn_32.Text == "O"
+             || btn_13.Text == "O" && btn_23.Text == "O" && btn_33.Text == "O")
+            {
+                Tocador.SoundLocation = "Error-Eletric.WAV";
+                Tocador.Play();
+                MessageBox.Show("O Jogador O Venceu", "Ganhou!!!",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fimdejogo();
+                return;
+            }
+
+            if (btn_11.Text == "O" && btn_22.Text == "O" && btn_33.Text == "O"
+             || btn_13.Text == "O" && btn_22.Text == "O" && btn_31.Text == "O")
+            {
+                Tocador.SoundLocation = "Error-Eletric.WAV";
+                Tocador.Play();
+                MessageBox.Show("O Jogador O Venceu", "Ganhou!!!",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fimdejogo();
+                return;
+            }
+
+            //Verificando se deu Velha
+
+            if (btn_11.Text != "" && btn_21.Text != "" && btn_31.Text != ""
+              && btn_12.Text != "" && btn_22.Text != "" && btn_32.Text != ""
+              && btn_13.Text != "" && btn_23.Text != "" && btn_33.Text != "")
+            {
+                Tocador.SoundLocation = "Error-Eletric.WAV";
+                Tocador.Play();
+                MessageBox.Show("Nenhum Jogador Venceu, deu Velha", "Empate",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fimdejogo();
+            }
+        }
+        
+        private void jogar(System.Windows.Forms.Button jogarXO)
+        {
+            jogarXO.Text = xo;
+
+            VerificandoStatus();
+
+            if (xo == "X") xo = "O";
+            else xo = "X";
+        }
+        
+        private void btn_11_Click(object sender, EventArgs e)
+        {
+            if (btn_11.Text == "") jogar(btn_11);
+        }
+
+        private void btn_12_Click(object sender, EventArgs e)
+        {
+            if (btn_12.Text == "") jogar(btn_12);
+        }
+
+        private void btn_13_Click(object sender, EventArgs e)
+        {
+            if (btn_13.Text == "") jogar(btn_13);
+        }
+
+        private void btn_21_Click(object sender, EventArgs e)
+        {
+            if (btn_21.Text == "") jogar(btn_21);
+        }
+
+        private void btn_22_Click(object sender, EventArgs e)
+        {
+            if (btn_22.Text == "") jogar(btn_22);
+        }
+
+        private void btn_23_Click(object sender, EventArgs e)
+        {
+            if (btn_23.Text == "") jogar(btn_23);
+        }
+
+        private void btn_31_Click(object sender, EventArgs e)
+        {
+            if (btn_31.Text == "") jogar(btn_31);
+        }
+
+        private void btn_32_Click(object sender, EventArgs e)
+        {
+            if (btn_32.Text == "") jogar(btn_32);
+        }
+
+        private void btn_33_Click(object sender, EventArgs e)
+        {
+            if (btn_33.Text == "") jogar(btn_33);
+        }
+
+        private void btn_NnovoJogo_Click(object sender, EventArgs e)
+        {
+            limpar();
+        }
+
+        private void Frm01_Load(object sender, EventArgs e)
+        {
+            Tocador.SoundLocation = "Error.WAV";
+            Tocador.Play();
+
+            //Tocador.SoundLocation = "Error.WAV";
+            //Tocador.PlayLooping();                      <-- Com playlooping toca o tempo com programa startado
+        }
+    }
+}
